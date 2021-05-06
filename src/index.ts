502d0eb2277cf1e045c80b363deb588607a5a13e
CodeNinja04 <hemendrasharma2019@gmail.com>
2021-05-06 20:01:04 +0530

    new site

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import './assets/favicon/favicon.ico'
+import './assets/ogimage.png'
+
+import 'normalize.css'
+import './styles/style.scss'
+
+import 'focus-visible'
+
+import './scripts/sw'
+import './scripts/analytics'
+import './scripts/cookies'
+import './scripts/hero'
+import './scripts/faq'
